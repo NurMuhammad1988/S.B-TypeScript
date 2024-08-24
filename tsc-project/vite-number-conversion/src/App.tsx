407c38178823d3c8afd5d6d6fb6a3dc295d55e36
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-08-24 23:21:53 +0500

    ddd

--- tsc-project/vite-number-conversion/src/App.tsx
+++ tsc-project/vite-number-conversion/src/App.tsx
@@ -1,15 +1,27 @@
-// import styles from "./styles-module.css" 
-// import * as styles from './styles-module.css';
-import styles from './styles.module.css'
+import { ChangeEvent, useState } from "react";
+import styles from "./styles.module.css"; //bu modul huddi shunday yozilishi kerak bo'lmasa ts buni tanimey qoladi 1 kun vaqt ketdi bu hato uchun chunki darsda bu modulni nomu home.module.css edi bu nom ishlamadi classlarni id bilan qo'yish uchun  tsx file ichida css modulni nomi ./styles.module.css bo'lishi shart bo'masa hina
 
-const App = (): JSX.Element => {//yani bu react cod tsda yozilgani uchun componentga yani funksiyaga bu funksiya qanday element ekanligi aytib qo'yilishi kerak
-  return (
-    <div className={styles.todo}>
-      
-      <h1 className={styles.title}>APP Todo</h1>
+const App = (): JSX.Element => {
+    //yani bu react cod tsda yozilgani uchun componentga yani funksiyaga bu funksiya qanday element ekanligi aytib qo'yilishi kerak tsx elemtni jsx elementdan farqi shu yani tsx react jsx file uchun moslashgan faqat nomi tsx qilingan typescript tsxni ichida bor
 
-    </div>
-  )
-}
+    const [title, setTitle] = useState<string>("")
 
-export default App
+    const changeHandler = (evt: ChangeEvent <HTMLInputElement>):void => {//void chunki hech narsa qaytarmaydi
+      setTitle(evt.target.value)
+    }
+
+    const handleSubmit = (): void =>{//void chunki hech narsa qaytarmaydi
+      console.log(title);
+    }
+
+    return (
+        <div className={styles.todo}>
+            <h1 className={styles.title}>APP Todo</h1>
+              <input className={styles.input} type="text" placeholder="Enter Todo" value={title} onChange={changeHandler}/>
+              <button className={styles.button} onClick={handleSubmit} >Add Todo</button>
+        </div>
+    );
+};
+
+export default App;
+
